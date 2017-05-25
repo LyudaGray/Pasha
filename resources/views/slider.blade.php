8dd23bf14eaa0673bb1b199055555eb2802c3825
Olga Zhizhka <helgaj@yandex.ru>
2017-05-26 00:08:50 +0300

    changes layout, styles, main.js

--- resources/views/slider.blade.php
+++ resources/views/slider.blade.php
@@ -1,4 +1,4 @@
-<script src="js/jquery.bxslider.min.js"></script><!--For Image Slider-->
+
 <div class="slide-wrap">
 
     <section class="slider">
@@ -15,28 +15,3 @@
 
     </section> <!--- End Animation -->
 </div>
-
-<script type="text/javascript">
-    $('.slider1').bxSlider({
-        mode: 'fade',
-        captions: false,
-        auto:true,
-        pager:false,
-
-    });
-    $('.slider2').bxSlider({
-        pager:false,
-        captions: true,
-        maxSlides: 3,
-        minSlides: 1,
-        slideWidth: 230,
-        slideMargin: 10
-    });
-    $('.slider3').bxSlider({
-        mode: 'fade',
-        captions: false,
-        auto:true,
-        pager:false,
-        controls:false,
-    });
-</script>
